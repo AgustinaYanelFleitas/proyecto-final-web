6dbaf271987a9b12915039a7632c9318f9bc320d
Agustina Yanel <>
2022-01-17 18:15:59 -0300

    initial configuration

---
+++ src/test/java/com/proyectofinalweb/proyectofinalweb/ProyectoFinalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinalweb.proyectofinalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
